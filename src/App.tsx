698d89dae99e7aca98b971f77fb91ecac0930c67
Rahat47313 <rahat47313@gmail.com>
2025-02-17 13:35:23 +0600

    show website under renovation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 function App() {
   return (
     <>
-      <p>Hello World!</p>
+      <div className="flex justify-between items-center h-screen w-[80%] mx-auto">
+        <div>
+          <p className="font-bold text-3xl">We are renovating!</p>
+          <p className="">Please visit us again soon</p>
+        </div>
+        <img src="/ComingSoon.png" className="h-[80%]" alt="Coming Soon" />
+      </div>
     </>
   );
 }
