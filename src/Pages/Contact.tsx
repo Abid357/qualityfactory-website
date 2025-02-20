9f666a87c5ed24d714c119ac926a4b02599c6efa
Rahat47313 <rahat47313@gmail.com>
2025-02-20 22:32:08 +0600

    comlete Contact form, add Card component

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -4,6 +4,7 @@ import { IoMail } from "react-icons/io5";
 import { BsMailbox2 } from "react-icons/bs";
 import { FaLocationDot } from "react-icons/fa6";
 import ContactForm from "../Components/Contact/ContactForm";
+import Card from "../Components/Card";
 
 export default function Contact() {
   return (
@@ -50,14 +51,18 @@ export default function Contact() {
             Address: Industrial Area, Al Thuoob, Umm Al Quwain, UAE
           </a>
         </div>
-        <div className="w-full aspect-[4/3] md:aspect-[16/9] mt-8">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3599.439931389467!2d55.6877521!3d25.557026499999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ef5e3e07a1c942d%3A0x31a071a3f5f7c0e9!2sQuality%20Refreshments%20Factory!5e0!3m2!1sen!2sbd!4v1739780386734!5m2!1sen!2sbd"
-            className="w-full h-full rounded-lg border-2 border-[#0C7E4A]"
-            allowFullScreen={true}
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          />
+        <div className="mt-10">
+          <Card>
+            <div className="flex justify-center items-center aspect-[4/3] md:aspect-[16/9] w-full h-full p-5">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3599.439931389467!2d55.6877521!3d25.557026499999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ef5e3e07a1c942d%3A0x31a071a3f5f7c0e9!2sQuality%20Refreshments%20Factory!5e0!3m2!1sen!2sbd!4v1739780386734!5m2!1sen!2sbd"
+                className="w-full h-full rounded-lg"
+                allowFullScreen={true}
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              />
+            </div>
+          </Card>
         </div>
         <div className="flex justify-center mt-10">
           <ContactForm />
