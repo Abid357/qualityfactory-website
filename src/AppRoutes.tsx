2565f2de2cb25d7f9dfdb00880db05a765ba313a
Rahat47313 <rahat47313@gmail.com>
2025-03-07 18:51:20 +0600

    Revert "Add Certificates page and Certifications section"
    This reverts commit 161fd624a9221e13a092ecef7e8b3a9e97c7706b.

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router";
 import Home from "./Pages/Home";
 import Catalog from "./Pages/Catalog";
-import Certificates from "./Pages/Certificates";
 
 export default function AppRoutes() {
   return (
@@ -9,7 +8,6 @@ export default function AppRoutes() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/catalog" element={<Catalog />} />
-        <Route path="/certificates" element={<Certificates />} />
       </Routes>
     </>
   );
