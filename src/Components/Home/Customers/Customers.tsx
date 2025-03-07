5451813e750a755c8a47c581c5e21bec396f8ec5
Rahat47313 <rahat47313@gmail.com>
2025-03-07 14:36:54 +0600

    fix Customer imgs and Slider blurBorder position

--- src/Components/Home/Customers/Customers.tsx
+++ src/Components/Home/Customers/Customers.tsx
@@ -1,5 +1,5 @@
-import Slider from "react-infinite-logo-slider";
-import Customerss from "./Customers.json";
+import Slider from "./Slider";
+import CustomersImage from "./Customers.json";
 
 export default function Customers() {
   return (
@@ -8,12 +8,16 @@ export default function Customers() {
         Meet our customers<span className="text-[#0C7E4A]">.</span>
       </p>
       <Slider blurBorders={true} blurBorderColor={"#f8f4f4"}>
-        {Customerss.map((customer, index) => (
-          <div key={index} className="flex items-center">
-            <Slider.Slide>
-              <img src={customer.image} alt="customer" className="w-36" />
-            </Slider.Slide>
+        {CustomersImage.map((customer, index) => (
+          <Slider.Slide key={index}>
+          <div className="flex items-center justify-center w-full h-full mx-5">
+            <img
+              src={customer.image}
+              alt="customer"
+              className="max-h-full max-w-full object-contain"
+            />
           </div>
+        </Slider.Slide>
         ))}
       </Slider>
     </>
