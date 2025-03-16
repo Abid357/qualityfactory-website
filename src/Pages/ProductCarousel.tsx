f314a2ea9209374e32a6c30722303fc22b4d6ffe
Rahat47313 <rahat47313@gmail.com>
2025-03-17 01:35:13 +0600

    include go back button in product carousel page

--- src/Pages/ProductCarousel.tsx
+++ src/Pages/ProductCarousel.tsx
@@ -1,11 +1,29 @@
+import { useNavigate } from "react-router";
 import Carousel from "../Components/Catalog/Carousel";
 
 export default function ProductCarousel() {
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate("/catalog");
+    window.scrollTo({
+      top: 0,
+      behavior: "auto",
+    });
+  };
   return (
     <>
       <div className="flex flex-col gap-40 w-[80%] pt-60 mb-20 mx-auto">
         <div id="carousel">
           <Carousel />
+        </div>
+        <div className="flex justify-center">
+          <button
+            onClick={handleBack}
+            className="text-white bg-[#0C7E4A] hover:bg-[#73C057] transition-all duration-300 ease-in-out transform active:scale-95 rounded-md w-fit px-3 py-2"
+          >
+            Go Back
+          </button>
         </div>
       </div>
     </>
