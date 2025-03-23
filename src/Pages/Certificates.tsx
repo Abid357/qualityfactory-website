692759568ff6af311574188bd33f99f54b500de2
Rahat47313 <rahat47313@gmail.com>
2025-03-23 20:33:11 +0600

    made suggested changes

--- src/Pages/Certificates.tsx
+++ src/Pages/Certificates.tsx
@@ -2,8 +2,8 @@ import CertificatesData from "../Components/Certificates/Certificates.json";
 
 export default function Certificates() {
   return (
-    <div className="w-[80%] mx-auto mb-10 pt-15 md:pt-40">
-      <div className="grid grid-cols-1 lg:grid-cols-2">
+    <div className="w-[80%] mx-auto mb-10 pt-20 md:pt-40">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
         {CertificatesData.map((cert, index) => (
           <div
             key={index}
