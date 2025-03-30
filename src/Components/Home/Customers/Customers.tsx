7c831eb7e03ccafe4b981d9a088580ed87f2027e
Rahat47313 <rahat47313@gmail.com>
2025-03-30 16:49:10 +0600

    customer slider in Home: faster & pauses on hover

--- src/Components/Home/Customers/Customers.tsx
+++ src/Components/Home/Customers/Customers.tsx
@@ -7,7 +7,7 @@ export default function Customers() {
       <p className="flex justify-center font-bold text-3xl lg:text-4xl xl:text-5xl whitespace-nowrap mb-5 lg:mb-10">
         Meet our customers<span className="text-[#0C7E4A]">.</span>
       </p>
-      <Slider blurBorders={true} blurBorderColor={"#f8f4f4"}>
+      <Slider blurBorders={true} blurBorderColor={"#f8f4f4"} duration={10} pauseOnHover={true}>
         {CustomersImage.map((customer, index) => (
           <Slider.Slide key={index}>
           <div className="flex items-center justify-center w-full h-full mx-5">
